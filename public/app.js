7ee6d3230be04537fabc8c0714d0fd1773faa692
MI55_H4NN4H <kang.hannah14@gmail.com>
2014-07-14 17:56:45 -0700

    fixed angular src issue

---
+++ public/app.js
@@ -0,0 +1,11 @@
+var todoApp = angular.module('todoApp', []);
+
+//todoApp.controller("ToDoCtrl", function ($scope) {
+//	$scope.todo = model;
+//});
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res) {
+ // res.render('index', { title: 'Express' });
+//});
+
+router.get('/', function (req, res) {
+	console.log(path.resolve(__dirname, '..', 'public'))
+ res.sendfile(path.join(path.resolve(__dirname, '..', 'public'), 'app.html'));
 });
 
 module.exports = router;
